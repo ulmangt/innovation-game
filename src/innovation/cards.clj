dcb160b8868dd4cf4b73e5d56ed13f3173909e8c
geoff.ulman <geoff.ulman@d44dd246-30e1-9cd2-caf8-ee020e120997>
2011-05-22 22:55:32 +0000

    added additional era 1 cards for testing purposes

--- src/innovation/cards.clj
+++ src/innovation/cards.clj
@@ -1,19 +1,44 @@
 (ns innovation.cards
   (:use (innovation core)))
 
-; create a test card
-(def astronomy
-  (struct-map card :id 1
-                   :name "Astronomy"
-                   :age 5
-                   :color :purple
-                   :symbols [:crown :bulb :bulb :hex]
-                   :dogma (fn [_] _)))
-
 (def city-states
-  (struct-map card :id 2
-                   :name "City States"
+  (struct-map card :name "City States"
                    :age 1
                    :color :purple
                    :symbols [:hex :crown :crown :castle]
+                   :dogma (fn [_] _)))
+
+(def the-wheel
+  (struct-map card :name "The Wheel"
+                   :age 1
+                   :color :green
+                   :symbols [:hex :castle :castle :castle]
+                   :dogma (fn [_] _)))
+
+(def mysticism
+  (struct-map card :name "Mysticism"
+                   :age 1
+                   :color :purple
+                   :symbols [:hex :castle :castle :castle]
+                   :dogma (fn [_] _)))
+
+(def writing
+  (struct-map card :name "Writing"
+                   :age 1
+                   :color :blue
+                   :symbols [:hex :bulb :bulb :crown]
+                   :dogma (fn [_] _))) 
+
+(def sailing
+  (struct-map card :name "Sailing"
+                   :age 1
+                   :color :green
+                   :symbols [:crown :crown :hex :leaf]
+                   :dogma (fn [_] _)))
+
+(def astronomy
+  (struct-map card :name "Astronomy"
+                   :age 5
+                   :color :purple
+                   :symbols [:crown :bulb :bulb :hex]
                    :dogma (fn [_] _)))
