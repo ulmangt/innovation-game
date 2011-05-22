79fbfc177bf7f4e951c90cef3e64980aa70a8495
geoff.ulman <geoff.ulman@d44dd246-30e1-9cd2-caf8-ee020e120997>
2011-05-22 06:27:38 +0000

    initial checkin

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject innovation "1.0.0-SNAPSHOT"
+  :description "A Clojure implementation of the card game Innovation."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
---
+++ test/innovation/test/core.clj
@@ -0,0 +1,6 @@
+(ns innovation.test.core
+  (:use [innovation.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
